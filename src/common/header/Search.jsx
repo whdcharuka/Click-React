d22b7015249b2f8ccca5d722f0ee9270ab026b21
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-07 22:48:13 +0530

    modified

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -24,7 +24,8 @@ const Search = ({ CartItem }) => {
           </div>
 
           <div className='icon f_flex width'>
-            <i className='fa fa-user icon-circle'></i>
+          <Link to='/User'><i className='fa fa-user icon-circle'></i>
+          </Link>
             <div className='cart'>
               <Link to='/cart'>
                 <i className='fa fa-shopping-bag icon-circle'></i>
