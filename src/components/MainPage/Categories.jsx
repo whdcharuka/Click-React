b4bda76ffb7006e49c1a9d0b24e2540e4b08a7d2
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-07 22:48:35 +0530

    modified

--- src/components/MainPage/Categories.jsx
+++ src/components/MainPage/Categories.jsx
@@ -1,50 +1,62 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Categories = () => {
   const data = [
     {
       cateImg: "./images/category/dress.png",
       cateName: "Fashion",
+      path:"fashion",
     },
     {
       cateImg: "./images/category/smartphone.png",
       cateName: "Electronic",
+      path:"ele",
     },
     {
       cateImg: "./images/category/car.png",
       cateName: "Cars",
+      path:"car",
     },
     {
       cateImg: "./images/category/house.png",
       cateName: "Home & Garden",
+      path:"home",
     },
     {
       cateImg: "./images/category/giftbox.png",
       cateName: "Gifts",
+      path:"gift",
     },
     {
       cateImg: "./images/category/guitar.png",
       cateName: "Music",
+      path:"music",
     },
     {
       cateImg: "./images/category/cosmetics.png",
       cateName: "Health & Beauty",
+      path:"health",
     },
     {
       cateImg: "./images/category/cat.png",
       cateName: "Pets",
+      path:"pet",
     },
     {
       cateImg: "./images/category/toys.png",
       cateName: "Baby Toys",
+      path:"baby",
     },
     {
       cateImg: "./images/category/essential-oil.png",
       cateName: "Groceries",
+      path:"gro",
     },
     {
       cateImg: "./images/category/education.png",
       cateName: "Books",
+      path:"book",
     },
   ]
 
@@ -54,8 +66,10 @@ const Categories = () => {
         {data.map((value, index) => {
           return (
             <div className='box f_flex' key={index}>
+              <Link to={value.path}>
               <img src={value.cateImg} alt='' />
               <span>{value.cateName}</span>
+              </Link>
             </div>
           )
         })}
