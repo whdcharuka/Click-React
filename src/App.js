2c02560c800c0b28994f8bb72b8fb0b841f645f4
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-07 22:48:28 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Data from "./components/Data"
 import Cart from "./common/Cart/Cart"
 import Footer from "./common/footer/Footer"
 import Sdata from "./components/shops/Sdata"
+import User from "./pages/User"
+import Contact from "./pages/Contact"
 
 function App() {
 
@@ -50,6 +52,10 @@ function App() {
           <Route path='/cart' exact>
             <Cart CartItem={CartItem} addToCart={addToCart} decreaseQty={decreaseQty} />
           </Route>
+        </Switch>
+        <Switch>
+          <Route path='/User' exact component={User} />
+          <Route path='/Contact' exact component={Contact} />
         </Switch>
         <Footer />
       </Router>
