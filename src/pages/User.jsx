326b37426fd5422d64a2b07f87d367c56af4f447
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-07 22:48:47 +0530

    modified

---
+++ src/pages/User.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "../common/header/Header.css"
+
+const User = ({ CartItem }) => {
+  return (
+    <>
+      <h1>Your Account</h1>
+    </>
+  )
+}
+
+export default User
