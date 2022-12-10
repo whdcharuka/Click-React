de334dbb357b19e589cd6924ba445c64bf3c3e97
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-10 08:58:31 +0530

    new

---
+++ src/components/DataGift.js
@@ -0,0 +1,75 @@
+const Data = {
+  giftItems: [
+    {
+      id: 1,
+      discount: 50,
+      cover: "./images/flash/flash-1.png",
+      name: "Shoes",
+      price: 100,
+    },
+    {
+      id: 2,
+      discount: 40,
+      cover: "./images/flash/flash-2.png",
+      name: "Watch",
+      price: 20,
+    },
+    {
+      id: 3,
+      discount: 40,
+      cover: "./images/flash/flash-3.png",
+      name: "Smart Mobile Black",
+      price: 200,
+    },
+    {
+      id: 4,
+      discount: 40,
+      cover: "./images/flash/flash-4.png",
+      name: "Smart Watch Black",
+      price: 50,
+    },
+    {
+      id: 5,
+      discount: 50,
+      cover: "./images/flash/flash-5.png",
+      name: "Shoes",
+      price: 100,
+    },
+    {
+      id: 6,
+      discount: 50,
+      cover: "./images/flash/flash-6.png",
+      name: "Shoes",
+      price: 100,
+    },
+    {
+      id: 7,
+      discount: 40,
+      cover: "./images/flash/flash-7.png",
+      name: "Smart Mobile Black",
+      price: 200,
+    },
+    {
+      id: 8,
+      discount: 40,
+      cover: "./images/flash/flash-8.png",
+      name: "Smart Watch Black",
+      price: 50,
+    },
+    {
+      id: 9,
+      discount: 50,
+      cover: "./images/flash/flash-9.png",
+      name: "Shoes",
+      price: 100,
+    },
+    {
+      id: 10,
+      discount: 50,
+      cover: "./images/flash/flash-10.png",
+      name: "Shoes",
+      price: 100,
+    },
+  ],
+}
+export default Data
