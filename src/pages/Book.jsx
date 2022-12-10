865292a826b6cb83d7d93632997f14ba82bdef30
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-10 08:57:34 +0530

    new

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,20 +1,20 @@
 import React from "react"
 import Home from "../components/MainPage/Home"
-import FlashDeals from "../components/flashDeals/FlashDeals"
+import FlashDeals1 from "../components/flashDeals/FlashDeals1"
 import TopCate from "../components/top/TopCate"
 import NewArrivals from "../components/newarrivals/NewArrivals"
 import Discount from "../components/discount/Discount"
 import Shop from "../components/shops/Shop"
 
-const Pages = ({ productItems, addToCart, CartItem, shopItems }) => {
+const Pages = ({ bookItems, addToCart, CartItem, shopItems }) => {
   return (
     <>
       <Home CartItem={CartItem} />
-      <FlashDeals productItems={productItems} addToCart={addToCart} />
-      <TopCate />
+      <FlashDeals1 productItems={bookItems} addToCart={addToCart} />
+      {/* <TopCate />
       <NewArrivals />
       <Discount />
-      <Shop shopItems={shopItems} addToCart={addToCart} />
+      <Shop shopItems={shopItems} addToCart={addToCart} /> */}
     </>
   )
 }
