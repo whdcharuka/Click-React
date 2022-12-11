4f1c7f7f5787d0386dd8f66e0154b579a8dfb96b
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-11 10:29:19 +0530

    modified

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,12 +1,32 @@
 import React from "react"
-import "../common/header/Header.css"
 
-const User = ({ CartItem }) => {
+const Wrapper = () => {
+  const data = [
+    {
+      cover: <i class="fa fa-user" aria-hidden="true"></i>,
+      title: "Your Account",
+      decs: "We offer competitive prices on our 100 million plus product any range.",
+    },
+  ]
   return (
     <>
-      <h1>Your Account</h1>
+      <section className='wrapper background'>
+        <div className='container grid3'>
+          {data.map((val, index) => {
+            return (
+              <div className='product1' key={index}>
+                <div className='img icon-circle'>
+                  <i>{val.cover}</i>
+                </div>
+                <h3>{val.title}</h3>
+                <p>{val.decs}</p>
+              </div>
+            )
+          })}
+        </div>
+      </section>
     </>
   )
 }
 
-export default User
+export default Wrapper
