c1f3d8dfe91a8afedb5046ef6fc4e800088dc56b
Dasuni Charuka <dasunicharuka4@gmail.com>
2022-12-11 10:30:00 +0530

    new

---
+++ src/pages/Terms.jsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+const Wrapper = () => {
+  const data = [
+    {
+      cover: "/images/term.jpg",
+      title: "Your Account",
+      decs: "We offer competitive prices on our 100 million plus product any range.",
+    },
+  ]
+  return (
+    <>
+      <section className='wrapper background'>
+        <div className='container grid3'>
+          {data.map((val, index) => {
+            return (
+              <div className='product1' key={index}>
+                <div>
+                  <img src={val.cover}></img>
+                </div>
+                <h3>{val.title}</h3>
+                <p>{val.decs}</p>
+              </div>
+            )
+          })}
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Wrapper
