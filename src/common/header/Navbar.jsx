f1159e723896a707f8b47818c0afb5c4ec4f2c2d
Dasuni Charuka <dasunicharuka4@gmail.com>
2023-01-08 23:03:49 +0530

    modified

--- src/common/header/Navbar.jsx
+++ src/common/header/Navbar.jsx
@@ -17,19 +17,12 @@ const Navbar = () => {
 
           <div className='navlink'>
             <ul className={MobileMenu ? "nav-links-MobileMenu" : "link f_flex capitalize"} onClick={() => setMobileMenu(false)}>
-              {/*<ul className='link f_flex uppercase {MobileMenu ? "nav-links-MobileMenu" : "nav-links"} onClick={() => setMobileMenu(false)}'>*/}
               <li>
                 <Link to='/'>home</Link>
               </li>
-              {/* <li>
-                <Link to='/pages'>pages</Link>
-              </li> */}
               <li>
                 <Link to='/user'>user account</Link>
               </li>
-              {/* <li>
-                <Link to='/vendor'>vendor account</Link>
-              </li> */}
               <li>
                 <Link to='/track'>track my order</Link>
               </li>
